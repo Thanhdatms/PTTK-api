e038f851a61ce4b30e168559b9c860a9128cae0b
Dang Thanh Dat <dangthanhdatuit@gmail.com>
2024-04-29 18:03:41 +0700

    update workflow

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,15 +6,12 @@ const { OK } = require('../helpers/index');
 class UserController {
     async get_list_users(req, res, next) {
         try {
-            // Assuming you want to fetch users using UserService
             const users = await UserService.getUsers();
-            // Sending a response with the fetched users
             res.status(200).json({
                 message: 'Users retrieved successfully',
                 data: users
             });
         } catch (error) {
-            // If an error occurs, pass it to the error-handling middleware
             next(error);
         }
     }
